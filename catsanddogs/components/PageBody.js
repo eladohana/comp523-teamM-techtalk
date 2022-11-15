20b7d151b66c6f497e9697e3ea4ac06c54f55ff6
Elad Ohana <elad.ohana@gmail.com>
2022-11-14 21:31:10 -0500

    Added dogs

--- catsanddogs/components/PageBody.js
+++ catsanddogs/components/PageBody.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react"
 import Button from "../components/Button"
 import Container from "../components/Container"
-import cat1 from "../public/cat1.jpg"
-import cat0 from "../public/cat0.jpg"
 
 const PageBody = ({title, images}) => {
   const [index, setIndex] = useState(0);
