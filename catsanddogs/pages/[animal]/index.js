e725e78eafe70d47c38132e3a45af2d8730fd9e2
Khang Tran <ktra@email.unc.edu>
2022-11-16 00:30:57 -0500

    error checking

--- catsanddogs/pages/[animal]/index.js
+++ catsanddogs/pages/[animal]/index.js
@@ -1,40 +1,40 @@
-import { useEffect, useState } from "react"
-import PageBody from "../../components/PageBody"
-import { useRouter } from "next/router"
+import { useEffect, useState } from "react";
+import PageBody from "../../components/PageBody";
+import { useRouter } from "next/router";
 
 const animals = () => {
-  const router = useRouter()
-  const { animal } = router.query
-  const [animals, setAnimals] = useState([])
-    
+  const router = useRouter();
+  const { animal } = router.query;
+  const [animals, setAnimals] = useState([]);
+
   const fetchData = async (url) => {
     try {
       const response = await fetch(url);
       const json = await response.json();
-      console.log(JSON.parse(json).map(x=> x.url))
+      console.log(JSON.parse(json).map((x) => x.url));
       // console.log(JSON.parse(json).map(x=> x.url))
-      setAnimals(JSON.parse(json).map(x => {
-        return x.url
-      }))
+      setAnimals(
+        JSON.parse(json).map((x) => {
+          return x.url;
+        })
+      );
       // import x from url
-      console.log(animals)
+      console.log(animals);
       // console.log(json);
-      
     } catch (error) {
       console.log("error", error);
     }
   };
 
-
-
-useEffect(() => {
-  const url = `/api/getpics/${animal}`;
-  fetchData(url);
+  useEffect(() => {
+    const url = `/api/getpics/${animal}`;
+    fetchData(url);
   }, [animal]);
+  if (animal && animals.length) {
+    return <PageBody title={animal} images={animals} />;
+  } else {
+    return <div>Loading image...</div>;
+  }
+};
 
-  return (
-    <PageBody title={animal} images={animals}/>
-  )
-}
-
-export default animals
+export default animals;
